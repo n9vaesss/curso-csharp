7cc1a273262cf84365279c3937b22ae02210a8c3
n9vaes <caioo24332433@gmail.com>
2024-10-24 17:03:52 -0300

    Adicionando teste da api do maps e adicionando controlerno api catalogo

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe6a964dcf39db0280b840f5a363b879ac60943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b1747a92c78405d79ea8ac70a0d24a2c6945f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
