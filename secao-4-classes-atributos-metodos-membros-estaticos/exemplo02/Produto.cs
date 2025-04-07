331f22728e95d4f01c740f8650f2b55d1450e4ec
n9vaes <caioo24332433@gmail.com>
2025-04-07 17:15:24 -0300

    adicionando exemplos e exercicios

---
+++ secao-4-classes-atributos-metodos-membros-estaticos/exemplo02/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo02
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+
+    }
+}
---
+++ secao-4-classes-atributos-metodos-membros-estaticos/exemplo02/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using exemplo02;
+using System.Globalization;
+
+Produto produto = new Produto();
+
+Console.WriteLine("Entre com os dados do produto:");
+
+Console.Write("Nome: ");
+produto.Nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantidade no estoque: ");
+produto.Quantidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados do produto: " + produto);
+
+Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+produto.AdicionarProdutos(quantidade);
+
+Console.WriteLine("Dados do produto: " + produto);
+
+Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+quantidade = int.Parse(Console.ReadLine());
+
+produto.RemoverProdutos(quantidade);
+
+Console.WriteLine("Dados do produto: " + produto);
+
+
+
+
