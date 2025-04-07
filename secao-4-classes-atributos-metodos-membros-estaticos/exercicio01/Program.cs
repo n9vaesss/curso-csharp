c9154799fc48f64e9ce9c1220416a04df64b428e
n9vaes <caioo24332433@gmail.com>
2025-04-07 16:16:55 -0300

    primeiro commit

---
+++ secao-4-classes-atributos-metodos-membros-estaticos/exercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using exercicio01;
+
+Pessoa x, y;
+
+x = new Pessoa();
+y = new Pessoa();
+
+Console.WriteLine("Dados da primeira pessoa: ");
+x.Nome = Console.ReadLine();
+x.Idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados da segunda pessoa: ");
+y.Nome = Console.ReadLine();
+y.Idade = int.Parse(Console.ReadLine());
+
+if(x.Idade > y.Idade)
+{
+    Console.WriteLine("Pessoa mais velha:" + x.Nome);
+}
+else
+{
+    Console.WriteLine("Pessoa mais velha:" + y.Nome);
+}
---
+++ secao-4-classes-atributos-metodos-membros-estaticos/exercicio02/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using exercicio02;
+using System.Globalization;
+
+Funcionario f1 = new Funcionario();
+Funcionario f2 = new Funcionario();
+
+Console.WriteLine("Dados do primeiro funcionário: ");
+f1.Nome = Console.ReadLine();
+f1.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.WriteLine("Dados do segundo funcionário: ");
+f2.Nome = Console.ReadLine();
+f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double mediaSalarios = (f1.Salario + f2.Salario) / 2;
+
+Console.WriteLine("Salário médio = " + mediaSalarios.ToString("F2", CultureInfo.InvariantCulture));
